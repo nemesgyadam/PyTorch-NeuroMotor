256af071b4ac3ddfbffbccfd0f0a926ee7306ab6
Nemes <nemesgyadam@gmail.com>
2023-08-03 12:46:41 +0200

    normalize linear layer inputs + layernorm

--- models/conditioned_eegnet.py
+++ models/conditioned_eegnet.py
@@ -2,9 +2,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from typing import Optional
+
 class EEGNet(nn.Module):
     def __init__(self, num_classes: int = 4, channels: int = 22, samples: int = 401,
-        dropout_rate: float = 0.5, kernel_length: int = 64, num_filters1: int = 16,
+        dropout_rate: float = 0.5, kernel_length: int = 64, ff_filter: int = 16,
         depth_multiplier: int = 2, num_filters2: int = 32, norm_rate: float = 0.25) -> None:
         super(EEGNet, self).__init__()
 
@@ -13,22 +15,22 @@ class EEGNet(nn.Module):
 
         # First convolutional block
         # Temporal convolutional to learn frequency filters
-        self.conv1 = nn.Conv2d(1, num_filters1, (1, kernel_length), padding=(0, kernel_length // 2), bias=False)
-        self.bn1 = nn.BatchNorm2d(num_filters1)
+        self.conv1 = nn.Conv2d(1, ff_filter, (1, kernel_length), padding=(0, kernel_length // 2), bias=False)
+        self.bn1 = nn.BatchNorm2d(ff_filter)
         
         # Depthwise convolutional block
         # Connected to each feature map individually, to learn frequency-specific spatial filters
-        self.dw_conv1 = nn.Conv2d(num_filters1, num_filters1 * depth_multiplier, (channels, 1), groups=num_filters1, bias=False)
-        self.bn2 = nn.BatchNorm2d(num_filters1 * depth_multiplier)
+        self.dw_conv1 = nn.Conv2d(ff_filter, ff_filter * depth_multiplier, (channels, 1), groups=ff_filter, bias=False)
+        self.bn2 = nn.BatchNorm2d(ff_filter * depth_multiplier)
         self.activation = nn.ELU()
         self.avg_pool1 = nn.AvgPool2d((1, 4))
         self.dropout1 = nn.Dropout(dropout_rate)
 
-        # Separable convolutional block
+        # Separable  convolutional block
         # Learns a temporal summary for each feature map individually, 
         # followed by a pointwise convolution, which learns how to optimally mix the feature maps together
-        self.sep_conv1 = nn.Conv2d(num_filters1 * depth_multiplier, num_filters1 * depth_multiplier, (1, 16), groups=num_filters1 * depth_multiplier, padding=(0, 8), bias=False)
-        self.conv2 = nn.Conv2d(num_filters1 * depth_multiplier, num_filters2, 1, bias=False)
+        self.sep_conv1 = nn.Conv2d(ff_filter * depth_multiplier, ff_filter * depth_multiplier, (1, 16), groups=ff_filter * depth_multiplier, padding=(0, 8), bias=False)
+        self.conv2 = nn.Conv2d(ff_filter * depth_multiplier, num_filters2, 1, bias=False)
         self.bn3 = nn.BatchNorm2d(num_filters2)
         self.avg_pool2 = nn.AvgPool2d((1, 8))
         
@@ -55,15 +57,19 @@ class EEGNet(nn.Module):
        
 
         x = self.flatten(x)
-    
 
         return x
+    
+    def calculate_output_dim(self) -> int:
+        x = torch.randn(1, 1, self.channels, self.samples)
+        x = self(x)
+        return x.shape[-1]
 
 class FeedForward(nn.Module):
-    def __init__(self, num_subjects: int = 1, num_filters1: int = 16):
+    def __init__(self, num_subjects: int = 1, ff_filter: int = 16):
         super(FeedForward, self).__init__()
         self.num_subjects = num_subjects
-        self.fn1 = nn.Linear(num_subjects, num_filters1)
+        self.fn1 = nn.Linear(num_subjects, ff_filter)
         self.act = nn.ELU()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -74,36 +80,48 @@ class FeedForward(nn.Module):
 
 class ConditionedEEGNet(nn.Module):
     def __init__(self, num_subjects: int = 1, num_classes: int = 4, channels: int = 22, samples: int = 401,
-        dropout_rate: float = 0.5, kernel_length: int = 64, num_filters1: int = 16,
-        depth_multiplier: int = 2, num_filters2: int = 32, norm_rate: float = 0.25, embed_dim: int = 32, num_filters3: int = 128) -> None:
+        dropout_rate: float = 0.5, kernel_length: int = 64, ff_filter: int = 16,
+        depth_multiplier: int = 2, num_filters2: int = 32, norm_rate: float = 0.25, embed_dim: int = 32, num_filters3: int = 128, init_std: Optional[float] = None) -> None:
         super(ConditionedEEGNet, self).__init__()
         self.embed_dim = embed_dim
-        #self.attention = nn.MultiheadAttention(embed_dim=self.embed_dim, num_heads=1, batch_first= True, dropout=0.2)
+        self.init_std = init_std
 
         self.eeg_processor = EEGNet(num_classes=num_classes, channels=channels, samples=samples,
-            dropout_rate=dropout_rate, kernel_length=kernel_length, num_filters1=num_filters1,
+            dropout_rate=dropout_rate, kernel_length=kernel_length, ff_filter=ff_filter,
             depth_multiplier=depth_multiplier, num_filters2=num_filters2, norm_rate=norm_rate)
-        self.subject_processor = FeedForward(num_subjects=num_subjects, num_filters1=num_filters1)
+        self.eeg_dim = self.eeg_processor.calculate_output_dim()
+        self.subject_processor = FeedForward(num_subjects=num_subjects, ff_filter=ff_filter)
 
+        self.subject_norm  = nn.LayerNorm(ff_filter)
+        self.eeg_norm = nn.LayerNorm(self.eeg_dim)
+        self.attn_norm = nn.LayerNorm(embed_dim)
 
-        self.query = nn.Linear(384, embed_dim, bias = False)
-        self.key = nn.Linear(16, embed_dim, bias = False)
-        #self.value = nn.Linear(16, embed_dim, bias = False)
-        self.value = nn.Linear(384, embed_dim, bias = False)
+        self.query = nn.Linear(self.eeg_dim, embed_dim, bias = False)    # TODO
+        self.key = nn.Linear(ff_filter, embed_dim, bias = False)       
+        #self.value = nn.Linear(ff_filter, embed_dim, bias = False)
+        self.value = nn.Linear(self.eeg_dim, embed_dim, bias = False)
         self.fn1 = nn.Linear(embed_dim, num_filters3)
         self.act1 = nn.ELU()
         self.dropout = nn.Dropout(dropout_rate)
         self.fn2 = nn.Linear(num_filters3, num_classes)
 
-        self.eeg_fn = nn.Linear(384, embed_dim)
+        self.eeg_fn = nn.Linear(self.eeg_dim, embed_dim)
+
+        if init_std is not None:
+            self.apply(self._init_weights)
 
 
     def forward(self, eeg_data: torch.Tensor, subject_id: torch.Tensor) -> torch.Tensor:
         subject_features = self.subject_processor(subject_id)
+        subject_features = self.subject_norm(subject_features)
+
         eeg_features = self.eeg_processor(eeg_data)
+        eeg_features = self.eeg_norm(eeg_features)
+
+
         #print(f'subject_features: {subject_features.shape}')
         #print(f'eeg_features: {eeg_features.shape}')
-        #x = self.attention(subject_features, eeg_features, eeg_features)
+
         Q = self.query(eeg_features)
         K = self.key(subject_features)
         #V = self.value(subject_features)
@@ -114,6 +132,7 @@ class ConditionedEEGNet(nn.Module):
         softmaxed_attn_matrix = F.softmax(normalized_attn_matrix, dim=-1)
 
         x = softmaxed_attn_matrix @ V
+        x = self.attn_norm(x)
         #print(f'x: {x.shape}')
         # Residual path
         eeg_features = self.eeg_fn(eeg_features)
@@ -124,3 +143,10 @@ class ConditionedEEGNet(nn.Module):
         x = self.dropout(x)
         x = self.fn2(x)
         return x
+    
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=self.init_std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+       
--- utils/model.py
+++ utils/model.py
@@ -1,5 +1,5 @@
 import torch 
-
+import torch.nn as nn
 def print_parameters(model):
     total_params = 0
     param_info = [(name, p.numel()) for name, p in model.named_parameters() if p.requires_grad]
@@ -10,3 +10,11 @@ def print_parameters(model):
         print(f'{name:.<{max_len}} --> {param}')
     
     print(f'\n{"Total Parameter Count:":.<{max_len}} --> {total_params}')
+
+def print_weights_statistics(model):
+    for name, layer in model.named_modules():
+        if isinstance(layer, nn.Linear):  # replace this with other types if you want to handle other layers
+            print(f"Layer: {name}")
+            print("Mean of weights: ", layer.weight.data.mean().item())
+            print("Std of weights: ", layer.weight.data.std().item())
+            print("--------------------")
